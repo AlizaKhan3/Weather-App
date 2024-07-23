8bdbf67e882f621c8ad9c37da498678ee998f973
Aliza Khan <alizaadi2@gmail.com>
2024-07-24 01:12:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -14,48 +14,80 @@ const cloudValue = document.getElementById("cloudValue");
 const uvValue = document.getElementById("uvValue");
 const pressureValue = document.getElementById("pressureValue");
 const forecast = document.querySelector(".forecast")
-
+const updates = document.querySelector(".updates");
 
 const API_KEY = '9f1d66f984eff1d963ba3dfeca9d0611';
-// const WEATHER_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather";
-// const WEATHER_DATA_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall";
-
-// function findUserLocation(){
-//     const apiUrl = `${WEATHER_API_ENDPOINT}?q=${userLocation.value}&appid=af9ee1243c88a0dceb4152ee480f69d6`;
-//     fetch(apiUrl)
-//    .then((res) => res.json())
-//    .then((data) => {
-//         if(data.cod!='' && data.cod!=200){ 
-//             alert(data.message);
-//             return;
-//         }
-//         console.log(data);
-//         const weatherDataUrl = `${WEATHER_DATA_ENDPOINT}?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=af9ee1243c88a0dceb4152ee480f69d6&exclude=minutely&units=metrics`;
-//         fetch(weatherDataUrl)
-//        .then((res) => res.json())
-//        .then((data) => {
-//             console.log(data);
-//         });
-//     });
-// }
-
 
 findUserLocation();
 function findUserLocation() {
     navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
-        let{lat, lon} = success.coords;
+        // console.log(success);
+        let{latitude, longitude} = success.coords;
 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`).then(res => res.json()).then(data => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
             console.log(data);
+            showWeatherData(data);
         })
     })
 }
 
+function showWeatherData(data){
+    let {weather, main, wind, sys, name} = data;
+    let {description} = weather[0];
+    let {temp, feels_like, humidity, pressure} = main;
+    let {speed} = wind;
+    let {sunrise, sunset} = sys;
+    let {all} = weather;
+    weatherIcon.innerHTML = ` <h2 class="temperature"> ${temp}</h2>
+                <div class="feelsLike"> ${feels_like}</div>
+                <div class="description"> ${description}</div>
+                <hr>
+                <div class="city"> ${name}</div>`
 
-
-
-
+    updates.innerHTML = ` <div class="card p-4" style="width: 18rem;">
+                    <h5 class="card-text humidity">Humidity</h5>
+                    <i class="fa-solid fa-water" style="color: rgba(245, 245, 245, 0.865);"></i>
+                    <div class="card-body">
+                        <h1 class="card-title" id="HumidityValue" style="color: #000;" >${humidity}</h1>
+                    </div>
+                </div>
+                <div class="card p-4" style="width: 18rem;">
+                    <h5 class="card-text humidity">Wind Speed</h5>
+                    <i class="fa-solid fa-wind" style="color: darkcyan;"></i>
+                    <div class="card-body">
+                        <h1 class="card-title" id="windSpeedValue">${speed}</h1>
+                    </div>
+                </div>
+                <div class="card p-2" style="width: 18rem;">
+                    <div class="card-body sun">
+                        <span class="my-3">
+                            <i class="fa-solid fa-sun" style="color: #f6d245;"></i>
+                            <p><span class="card-title" id="sunRiseValue">${sunrise}</span>Sunrise</p>
+                        </span>
+                        <span class="my-3">
+                            <i class="fa-solid fa-cloud-sun" style="color: #f6d245;"></i>
+                            <p><span class="card-title" id="sunSetValue">${sunset}</span>Sunset</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="card p-4" style="width: 18rem;">
+                    <h5 class="card-text clouds">Clouds</h5>
+                    <i class="fa-solid fa-cloud" style="color: grey;"></i>
+                    <div class="card-body">
+                        <h1 class="card-title" id="cloudValue">${weather[0].clouds}%</h1>
+                    </div>
+                </div>
+                
+                <div class="card p-4" style="width: 18rem;">
+                    <h5 class="card-text pressure">Pressure</h5>
+                    <i class="fa-solid fa-volcano" style="color: darkslategrey;"></i>
+                    <div class="card-body">
+                        <h1 class="card-title" id="pressureValue">${pressure}</h1>
+                    </div>
+                </div>`
+}
 
 
 // const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
@@ -109,3 +141,33 @@ function findUserLocation() {
 //     });
 // });
 // finduserLoaction();
+
+
+
+
+
+
+
+
+
+// const WEATHER_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather";
+// const WEATHER_DATA_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall";
+
+// function findUserLocation(){
+//     const apiUrl = `${WEATHER_API_ENDPOINT}?q=${userLocation.value}&appid=af9ee1243c88a0dceb4152ee480f69d6`;
+//     fetch(apiUrl)
+//    .then((res) => res.json())
+//    .then((data) => {
+//         if(data.cod!='' && data.cod!=200){ 
+//             alert(data.message);
+//             return;
+//         }
+//         console.log(data);
+//         const weatherDataUrl = `${WEATHER_DATA_ENDPOINT}?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=af9ee1243c88a0dceb4152ee480f69d6&exclude=minutely&units=metrics`;
+//         fetch(weatherDataUrl)
+//        .then((res) => res.json())
+//        .then((data) => {
+//             console.log(data);
+//         });
+//     });
+// }
